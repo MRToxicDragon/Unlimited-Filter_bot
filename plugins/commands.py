4d89ed72f51563c6115caef64c9a09440103e716
appu2006 <93041202+appu2006@users.noreply.github.com>
2022-01-19 13:45:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("father", url="https://t.me/MRToxicDragon"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Channel", url="https://t.me/TMC_BOTX"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/TMC_BOTX_Group")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/+C_RwBhk_0bhkZWVl")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
